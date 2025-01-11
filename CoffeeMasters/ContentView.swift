8a429d9b043c526d28a685efd4f039f29a680224
Lewis Sneddon <61014463+sneddonlewis@users.noreply.github.com>
2025-01-11 20:38:27 +0000

    added tabs

--- CoffeeMasters/ContentView.swift
+++ CoffeeMasters/ContentView.swift
@@ -10,7 +10,31 @@ import SwiftUI
 struct ContentView: View {
     
     var body: some View {
-        OffersPage()
+        TabView {
+            Text("Home")
+                .tabItem {
+                    Image(systemName: "cup.and.saucer")
+                    Text("Home")
+                }
+            
+            OffersPage()
+                .tabItem {
+                    Image(systemName: "tag")
+                    Text("Offers")
+                }
+            
+            Text("Order")
+                .tabItem {
+                    Image(systemName: "cart")
+                    Text("Order")
+                }
+            
+            Text("Info")
+                .tabItem {
+                    Image(systemName: "info")
+                    Text("Info")
+                }
+        }
     }
 }
 
--- CoffeeMasters/OffersPage.swift
+++ CoffeeMasters/OffersPage.swift
@@ -9,10 +9,12 @@ import SwiftUI
 
 struct OffersPage: View {
     var body: some View {
-        VStack {
-            Offer(title: "Early Coffee", description: "10% off. Offer valid 6am to 9am.")
-            Offer(title: "Welcome Gift", description: "25% off on your first order.")
-        }
+        NavigationView {
+            List {
+                Offer(title: "Early Coffee", description: "10% off. Offer valid 6am to 9am.")
+                Offer(title: "Welcome Gift", description: "25% off on your first order.")
+            }
+        }.navigationTitle("Offers")
     }
 }
 
