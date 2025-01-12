3843ebea12c20492e336449fa5d4bf97131377cc
Lewis Sneddon <61014463+sneddonlewis@users.noreply.github.com>
2025-01-12 12:55:10 +0000

    updated order manager

--- CoffeeMasters/Model/OrderManager.swift
+++ CoffeeMasters/Model/OrderManager.swift
@@ -9,4 +9,14 @@ import Foundation
 
 class OrderManager: ObservableObject {
     @Published var products: [(Product, Int)] = []
+    
+    func add(product: Product, quantity: Int) {
+        self.products.append( (product, quantity) )
+    }
+    
+    func remove(product: Product) {
+        self.products.removeAll(where: { item in
+            return item.0.id == product.id
+        })
+    }
 }
