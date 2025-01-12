0e1969554c3f6517b9ad5e46ae217a153a0fc83c
Lewis Sneddon <61014463+sneddonlewis@users.noreply.github.com>
2025-01-12 13:42:09 +0000

    just missing launch screen/icon etc.

--- CoffeeMasters/PageViews/InfoPage.swift
+++ CoffeeMasters/PageViews/InfoPage.swift
@@ -6,10 +6,37 @@
 //
 
 import SwiftUI
+import WebKit
+
+class WebInterfaceDialogs: NSObject, WKScriptMessageHandler {
+    func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
+        print(message.body)
+    }
+}
+
+struct WebView: UIViewRepresentable {
+    func makeUIView(context: Context) -> WKWebView {
+        let config = WKWebViewConfiguration()
+        config.userContentController.add(WebInterfaceDialogs() , name: "alert")
+        return WKWebView(frame: .zero, configuration: config)
+    }
+    
+    func updateUIView(_ webView: WKWebView, context: Context) {
+        let url = URL(string: "https://firtman.github.io/coffeemasters/webapp")!
+        webView.load(URLRequest(url: url))
+        
+        let name = UserDefaults.standard.string(forKey: "name") ?? ""
+        webView.evaluateJavaScript("injectFormData('\(name)')")
+    }
+    
+    func executeJavaScript(webView: WKWebView) {
+        webView.evaluateJavaScript("alert('hello from Swift')")
+    }
+}
 
 struct InfoPage: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        WebView()
     }
 }
 
