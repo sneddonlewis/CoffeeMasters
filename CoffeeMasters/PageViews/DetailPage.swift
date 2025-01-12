757ad8539ea3a1ffbeb70afc1eb503a69918d5eb
Lewis Sneddon <61014463+sneddonlewis@users.noreply.github.com>
2025-01-12 09:25:29 +0000

    augmented data model

--- CoffeeMasters/PageViews/DetailPage.swift
+++ CoffeeMasters/PageViews/DetailPage.swift
@@ -8,11 +8,39 @@
 import SwiftUI
 
 struct DetailPage: View {
+    @State var quantity = 0
+    
     var body: some View {
-        NavigationView {
-            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            Image("DummyImage")
+                .cornerRadius(5)
+                .frame(maxWidth: .infinity, idealHeight: 150, maxHeight: 150)
+                .padding(.top, 32)
+            Text("Product")
+                .frame(maxWidth: .infinity)
+                .multilineTextAlignment(.leading)
+                .padding(24)
+            HStack {
+                Text("$ 4.25 ea")
+                Stepper(value: $quantity, in: 1...10) { }
+            }
+                .frame(maxWidth: .infinity)
+                .padding(30)
+                            
+            Text("Subtotal $4.25")
+                .bold()
+                .padding(12)
+            
+            Button("Add \(quantity) to Cart") {
+                //TODO
+            }
+                .padding()
+                .frame(width: 250.0)
+                .background(Color("Alt2"))
+                .foregroundColor(Color.black)
+                .cornerRadius(25)
+
         }
-        .navigationTitle("Details")
     }
 }
 
